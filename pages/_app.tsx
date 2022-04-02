7a02fbc6713d28c20a1b520bfee2436d752bf55f
seungwon2 <dandelionsw2@ewhain.net>
2022-04-02 14:24:04 +0900

    :building_construction: [Build] : build with next.js & typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/** @format */
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<GlobalStyle />
+			<Head>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<title>Da0</title>
+			</Head>
+			<>
+				<Component {...pageProps} />
+			</>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
