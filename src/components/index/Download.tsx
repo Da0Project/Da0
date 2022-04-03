360e7ec5a69b0c408f44f4cd80d15b45ac241ceb
seungwon2 <dandelionsw2@ewhain.net>
2022-04-03 14:38:47 +0900

    :bug: [Fix] : fix bottom graphic margin

--- src/components/index/Download.tsx
+++ src/components/index/Download.tsx
@@ -14,7 +14,9 @@ export default function Dowload() {
 			<InnerWrapper>
 				<Button>Button</Button>
 			</InnerWrapper>
-			<Graphic />
+			<InnerWrapper>
+				<Graphic />
+			</InnerWrapper>
 		</Wrapper>
 	);
 }
